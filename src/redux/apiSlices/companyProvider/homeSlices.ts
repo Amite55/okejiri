a584d1a9c10ccf71400fa12d2d572174785620f6
MHDabdulkader <abdulkader201025@gmail.com>
2025-11-09 09:19:42 +0600

    Provider Company home

---
+++ src/redux/apiSlices/companyProvider/homeSlices.ts
@@ -0,0 +1,37 @@
+import { api } from "../../base/baseApi";
+
+export const companyProviderHomeSlice = api.injectEndpoints({
+    endpoints:(builder)=>({
+        homeData: builder.query({
+            query: (filter= "this_week") =>({
+                url:`home-data?filter=${filter}`,
+                
+                method: "GET"
+            }),
+            providesTags:["order"],
+        }),
+        recentTransactions: builder.query({
+            query: (per_page = 10)=>({
+                url: `my-transactions?per_page=10`,
+                method: "GET"
+            }),
+            providesTags:["order"]
+        }),
+        recentOrder: builder.query({
+            query:()=>({
+                url: `get-provider-orders`,
+                method: "GET"
+            }),
+            providesTags: ["order"]
+        })
+
+    }),
+    overrideExisting:true
+})
+
+
+export const {
+    useHomeDataQuery,
+    useRecentTransactionsQuery,
+    useRecentOrderQuery
+} = companyProviderHomeSlice; 
