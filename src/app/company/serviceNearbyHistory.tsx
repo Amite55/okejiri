b00b926b1d195dbb56b4d2a0471a5f84d8aa0a15
Amite55 <amite403@gmail.com>
2025-11-13 18:20:41 +0600

    today complete refar and my service history and wallet

--- src/app/company/serviceNearbyHistory.tsx
+++ src/app/company/serviceNearbyHistory.tsx
@@ -2,34 +2,52 @@ import ServiceCard from "@/src/Components/ServiceCard";
 import ServiceCardSkeleton from "@/src/Components/skeletons/ServiceCardSkeleton";
 import BackTitleButton from "@/src/lib/HeaderButtons/BackTitleButton";
 import tw from "@/src/lib/tailwind";
-import { usePackagesByServiceIdQuery } from "@/src/redux/apiSlices/userProvider/servicesSlices";
-import { router, useLocalSearchParams } from "expo-router";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { useServiceNearbyQuery } from "@/src/redux/apiSlices/userProvider/homeSlices";
+import { router } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
 const ServiceNearbyHistory = () => {
-  const { categoryService, service_id } = useLocalSearchParams();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [hasMorePages, setHasMorePages] = useState<boolean>(true);
+  const [nearByData, setNearByData] = useState<any[]>([]);
 
   // ------------------ api end point ------------------
-  const { data: PackagesByServiceData, isLoading } =
-    usePackagesByServiceIdQuery(Number(service_id));
-
-  // ------------------ loading skeleton ------------------
-  if (isLoading) {
+  const {
+    data: nearByServiceData,
+    isLoading: isNearByServiceLoading,
+    isFetching,
+  } = useServiceNearbyQuery({ per_page: 10, page: currentPage });
+  // -------------------- pagination ---------------------
+  useEffect(() => {
+    if (nearByServiceData?.data?.data?.length > 0) {
+      setNearByData((prev) => [...prev, ...nearByServiceData.data.data]);
+    } else {
+      setHasMorePages(false);
+    }
+  }, [nearByServiceData]);
+  // --------------------- skeleton ---------------------
+  if (isFetching && currentPage === 1) {
     return <ServiceCardSkeleton />;
   }
-  // =================== no data found ==================
-  if (PackagesByServiceData?.data?.packages?.length === 0) {
+  // ------------------ when no data found ------------------
+  if (!isFetching && nearByData.length === 0) {
     return (
       <View style={tw`flex-1 items-center justify-center`}>
-        <Text style={tw`text-lg text-primary`}>No data found</Text>
+        <Text style={tw`text-lg text-primary`}>No service data found</Text>
       </View>
     );
   }
-
   return (
     <FlatList
-      data={PackagesByServiceData?.data?.packages}
+      onEndReachedThreshold={0.3}
+      onEndReached={() => {
+        if (hasMorePages && !isFetching) {
+          setCurrentPage((prev) => prev + 1);
+        }
+      }}
+      // data={nearByServiceData?.data?.data}
+      data={nearByData}
       renderItem={({ item, index }: any) => (
         <ServiceCard
           item={item}
@@ -44,15 +62,21 @@ const ServiceNearbyHistory = () => {
       )}
       ListHeaderComponent={() => (
         <BackTitleButton
-          pageName={categoryService ? "Booking" : "Services nearby"}
+          pageName={"Services nearby"}
           onPress={() => router.back()}
           titleTextStyle={tw`text-xl`}
         />
       )}
       keyExtractor={(item, index) => (item.id || item._id || index).toString()}
-      contentContainerStyle={tw`bg-base_color flex-1 px-5 gap-3 pb-10`}
+      contentContainerStyle={tw`bg-base_color  px-5 gap-3 pb-10`}
       showsVerticalScrollIndicator={false}
+      style={tw`flex-1 bg-base_color`}
       showsHorizontalScrollIndicator={false}
+      ListFooterComponent={() =>
+        isFetching && hasMorePages ? (
+          <ActivityIndicator size="large" color="#FF6600" style={tw`my-5`} />
+        ) : null
+      }
     />
   );
 };
