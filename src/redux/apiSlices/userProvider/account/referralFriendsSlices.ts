deccfbf867782fde05c125dc758b4a8ffc1670b4
SamyaRoy999 <samyaroy999@gmail.com>
2025-11-17 09:45:01 +0600

    refer a friend

--- src/redux/apiSlices/userProvider/account/referralFriendsSlices.ts
+++ src/redux/apiSlices/userProvider/account/referralFriendsSlices.ts
@@ -3,8 +3,8 @@ import { api } from "@/src/redux/base/baseApi";
 export const referralFriendsSlices = api.injectEndpoints({
   endpoints: (build) => ({
     getMyReferrals: build.query({
-      query: ({ per_page, page }) => ({
-        url: `/my-referrals?per_page=${per_page}&page=${page}`,
+      query: (page) => ({
+        url: `/my-referrals&page=${page}`,
         method: "GET",
       }),
     }),
