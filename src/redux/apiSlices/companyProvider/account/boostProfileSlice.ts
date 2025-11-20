3e1cd5fdaca97079f5925a5fd34afe5106ffb8ac
Amite55 <amite403@gmail.com>
2025-11-20 18:37:18 +0600

    complete and send test pash

--- src/redux/apiSlices/companyProvider/account/boostProfileSlice.ts
+++ src/redux/apiSlices/companyProvider/account/boostProfileSlice.ts
@@ -1,37 +1,35 @@
 import { api } from "@/src/redux/base/baseApi";
 
 export const companyBoostProfileSlice = api.injectEndpoints({
-    endpoints: (builder)=>({
-        boostMyProfilePost: builder.mutation({
-            query: (requestBody) =>({
-                url: "/boost-my-profile",
-                method: "POST",
-                body: requestBody
-            }),
-            invalidatesTags: ["settings"]
-        }) ,
-        boostMyProfileGet: builder.query({
-            query: ()=>({
-                url: "/boost-my-profile",
-                method: "GET"
-            }),
-            providesTags: ["settings"]
-        }),
-        getSetting: builder.query({
-            query: ()=>({
-                url: "/get-settings",
-                method: "GET"
-            }),
-            providesTags: ["settings"]
-        }),
-
-
-    })
-})
+  endpoints: (builder) => ({
+    boostMyProfilePost: builder.mutation({
+      query: (requestBody) => ({
+        url: "/boost-my-profile",
+        method: "POST",
+        body: requestBody,
+      }),
+      invalidatesTags: ["boost"],
+    }),
+    boostMyProfileGet: builder.query({
+      query: () => ({
+        url: "/boost-my-profile",
+        method: "GET",
+      }),
+      providesTags: ["boost"],
+    }),
+    getSetting: builder.query({
+      query: () => ({
+        url: "/get-settings",
+        method: "GET",
+      }),
+      providesTags: ["boost"],
+    }),
+  }),
+});
 
 export const {
-    useBoostMyProfileGetQuery,
-    useBoostMyProfilePostMutation,
-    useGetSettingQuery,
-    // useLazyGetSettingQuery
-}= companyBoostProfileSlice;
+  useBoostMyProfileGetQuery,
+  useBoostMyProfilePostMutation,
+  useGetSettingQuery,
+  // useLazyGetSettingQuery
+} = companyBoostProfileSlice;
