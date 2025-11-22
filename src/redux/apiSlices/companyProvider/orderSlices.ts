97962d8c1b5ca3b8da1fb028dc86066ec4d57d54
Amite55 <amite403@gmail.com>
2025-11-22 19:52:37 +0600

    delete unused console

--- src/redux/apiSlices/companyProvider/orderSlices.ts
+++ src/redux/apiSlices/companyProvider/orderSlices.ts
@@ -1,95 +1,87 @@
 import { api } from "../../base/baseApi";
 
 export const companyProviderOrderSlice = api.injectEndpoints({
-    endpoints: (builder) => ({
-        orderDetails: builder.query({
-            query: (id) => ({
-                url: `/order-details/${id}`,
-                method: "GET"
-            }),
-            providesTags: ["order"]
-        }),
-        getAllProviderOrders: builder.query({
-            query: ()=>({
-                url: "/get-provider-orders",
-                method: "GET"
-            }),
-            providesTags: ["order"]
-        }),
-        getProviderOrders: builder.query({
-            query: ({status, booking_process="instant"}) => {
-                let queryString;
-                if(status && booking_process){
-                    queryString = `status=${status}&booking_process=${booking_process}`;
-                }
-                else if(status){
-                    queryString = `status=${status}&booking_process=${booking_process}`;
-                }
-                
-                // const queryString = new URLSearchParams(params).toString();
-                console.log("Query string ========== ", queryString!)
-                return {
-                    url: queryString
-                        ? `/get-provider-orders?${queryString}`
-                        : `/get-provider-orders`,
-                    method: "GET"
-                }
-            },
-            providesTags:["order"]
-        }),
-        requestExtendDeliveryTime: builder.mutation({
-            query: (reasons)=>({
-                url: "/request-extend-delivery-time",
-                method:"POST",
-                body: reasons
-            }),
-            invalidatesTags:["order"]
-        }),
-        orderApprove: builder.mutation({
-            query:(id)=>({
-                url: `/order-approve/${id}`,
-                method: "POST"
-            }),
-            invalidatesTags:["order"]
-        }),
-        addDispute: builder.mutation({
-            query: (dispute)=>({
-                url:"/add-dispute",
-                method: "POST",
-                body: dispute
-            }),
-            invalidatesTags:["dispute"]
-        }),
-        orderReject: builder.mutation({
-            query: (id)=>({
-                url: `/order-reject/${id}`,
-                method: "POST",
-            }),
-            invalidatesTags: ["order"]
-        }),
-        requestForDelivery:builder.mutation({
-            query:(id)=>({
-                url: `/request-for-delivery/${id}`,
-                method: "POST"
-            })
-        }),
-        
-
-
+  endpoints: (builder) => ({
+    orderDetails: builder.query({
+      query: (id) => ({
+        url: `/order-details/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["order"],
     }),
-    overrideExisting: true
-})
+    getAllProviderOrders: builder.query({
+      query: () => ({
+        url: "/get-provider-orders",
+        method: "GET",
+      }),
+      providesTags: ["order"],
+    }),
+    getProviderOrders: builder.query({
+      query: ({ status, booking_process = "instant" }) => {
+        let queryString;
+        if (status && booking_process) {
+          queryString = `status=${status}&booking_process=${booking_process}`;
+        } else if (status) {
+          queryString = `status=${status}&booking_process=${booking_process}`;
+        }
 
+        return {
+          url: queryString
+            ? `/get-provider-orders?${queryString}`
+            : `/get-provider-orders`,
+          method: "GET",
+        };
+      },
+      providesTags: ["order"],
+    }),
+    requestExtendDeliveryTime: builder.mutation({
+      query: (reasons) => ({
+        url: "/request-extend-delivery-time",
+        method: "POST",
+        body: reasons,
+      }),
+      invalidatesTags: ["order"],
+    }),
+    orderApprove: builder.mutation({
+      query: (id) => ({
+        url: `/order-approve/${id}`,
+        method: "POST",
+      }),
+      invalidatesTags: ["order"],
+    }),
+    addDispute: builder.mutation({
+      query: (dispute) => ({
+        url: "/add-dispute",
+        method: "POST",
+        body: dispute,
+      }),
+      invalidatesTags: ["dispute"],
+    }),
+    orderReject: builder.mutation({
+      query: (id) => ({
+        url: `/order-reject/${id}`,
+        method: "POST",
+      }),
+      invalidatesTags: ["order"],
+    }),
+    requestForDelivery: builder.mutation({
+      query: (id) => ({
+        url: `/request-for-delivery/${id}`,
+        method: "POST",
+      }),
+    }),
+  }),
+  overrideExisting: true,
+});
 
 export const {
-    useLazyOrderDetailsQuery,
-    useOrderDetailsQuery,
-    useLazyGetProviderOrdersQuery,
-    useRequestExtendDeliveryTimeMutation,
-    useOrderApproveMutation,
-    useAddDisputeMutation,
-    useOrderRejectMutation,
-    useRequestForDeliveryMutation,
-    useGetAllProviderOrdersQuery,
-    
-} = companyProviderOrderSlice; 
+  useLazyOrderDetailsQuery,
+  useOrderDetailsQuery,
+  useLazyGetProviderOrdersQuery,
+  useRequestExtendDeliveryTimeMutation,
+  useOrderApproveMutation,
+  useAddDisputeMutation,
+  useOrderRejectMutation,
+  useRequestForDeliveryMutation,
+  useGetAllProviderOrdersQuery,
+} = companyProviderOrderSlice;
